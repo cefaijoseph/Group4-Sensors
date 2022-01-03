6b1db9246d6bd05474c465e2f922d28b33cbd29d
Joseph Cefai <cefai2001@gmail.com>
2022-01-03 18:38:29 +0100

    Pedometer update

--- Pedometer.js
+++ Pedometer.js
@@ -2,89 +2,59 @@ import React, { useState, useEffect, Component } from 'react';
 import { StyleSheet, StatusBar, Text, View } from 'react-native';
 import { Pedometer } from 'expo-sensors';
 
-const PedometerScreen=()=>{
-    state = {
-        isPedometerAvailable: 'checking',
-        pastStepCount: 0,
-        currentStepCount: 0,
-      };
+const PedometerScreen = () => {
+    const [pastStepCount, setPastStepCount] = useState(0);
+    const [currentStepCount, setCurrentStepCount] = useState(0);
+    const [isPedometerAvailable, setIsPedometerAvailable] = useState(false);
+    const [subscription, setSubscription] = useState(null);
 
-        const [subscription, setSubscription] = useState(null);
+    useEffect(() => {
+        _subscribe();
+        return () => _unsubscribe();
+    }, []);
 
-        const _subscribe = () => {
-            this._subscription = Pedometer.watchStepCount(result => {
-                this.setState({
-                  currentStepCount: result.steps,
-                });
-              });
-          
-              Pedometer.isAvailableAsync().then(
+    const _subscribe = () => {
+            Pedometer.watchStepCount(result => {
+                setCurrentStepCount(result.steps)
+            }),
+            Pedometer.isAvailableAsync().then(
                 result => {
-                  this.setState({
-                    isPedometerAvailable: String(result),
-                  });
+                    setIsPedometerAvailable(result)
                 },
                 error => {
-                  this.setState({
-                    isPedometerAvailable: 'Could not get isPedometerAvailable: ' + error,
-                  });
+                    this.setState({
+                        isPedometerAvailable: 'Could not get isPedometerAvailable: ' + error,
+                    });
                 }
-              );
-          
-              const end = new Date();
-              const start = new Date();
-              start.setDate(end.getDate() - 1);
-              Pedometer.getStepCountAsync(start, end).then(
-                result => {
-                  this.setState({ pastStepCount: result.steps });
-                },
-                error => {
-                  this.setState({
-                    pastStepCount: 'Could not get stepCount: ' + error,
-                  });
-                }
-              );
-            };
-
-        const _unsubscribe = () => {
-            subscription && subscription.remove();
-            setSubscription(null);
-        };
-
-        useEffect(() => {
-            _subscribe();
-            return () => _unsubscribe();
-        }, []);
-
-        return (
-            <View style={styles.container}>
-                <View style={styles.PedometerContainer}>
-                    <Text style={styles.text}>Accelerometer</Text>
-                    <Text>Pedometer connection: {this.state.isPedometerAvailable}</Text>
-                    <Text>Steps taken in the last 24 hours: {this.state.pastStepCount}</Text>
-                    <Text>Walk! And watch this go up: {this.state.currentStepCount}</Text>
-                </View>
-            </View>
-        );
-    }
+            )
     
-const styles = StyleSheet.create({
-    container: {
-        paddingTop: (StatusBar.currentHeight),
-        flex: 1,
-    },
-    text: {
-        fontSize: 20
-    },
-    textValues: {
-        flexDirection: 'column'
-    },
-    PedometerContainer: {
-        flex: 1,
-        alignItems: 'center',
-        flexDirection: 'column',
-        justifyContent: 'center',
-    }
-})
+            const end = new Date();
+            const start = new Date();
+            start.setDate(end.getDate() - 1);
+            Pedometer.getStepCountAsync(start, end).then(
+                result => {
+                    setPastStepCount(result.steps)
+                },
+                error => {
+                    this.setState({
+                        pastStepCount: 'Could not get stepCount: ' + error,
+                    });
+                }
+            );
+    };
+
+    const _unsubscribe = () => {
+        subscription && subscription.remove();
+        setSubscription(null);
+    };
+
+    return (
+        <View >
+            <Text>Pedometer connection: {isPedometerAvailable ? 'Connected' : 'Not connected'}</Text>
+            <Text>Steps taken in the last 24 hours: {pastStepCount}</Text>
+            <Text>Walk! And watch this go up: {currentStepCount}</Text>
+        </View>
+    );
+}
 
 export default PedometerScreen;
