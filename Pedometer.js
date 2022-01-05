2a1605e5eb9adff697710ff0f9b87072559e7669
Joseph Cefai <cefai2001@gmail.com>
2022-01-05 11:10:04 +0100

    Pedometer comments

--- Pedometer.js
+++ Pedometer.js
@@ -3,53 +3,53 @@ import { StyleSheet, StatusBar, Text, View, Platform } from 'react-native';
 import { Pedometer } from 'expo-sensors';
 
 const PedometerScreen = () => {
-    const [pastStepCount, setPastStepCount] = useState(0);
-    const [currentStepCount, setCurrentStepCount] = useState(0);
-    const [isPedometerAvailable, setIsPedometerAvailable] = useState(false);
+    // const [pastStepCount, setPastStepCount] = useState(0);
+    // const [currentStepCount, setCurrentStepCount] = useState(0);
+    // const [isPedometerAvailable, setIsPedometerAvailable] = useState(false);
 
     useEffect(() => {
-        _subscribe();
+        // _subscribe();
     }, []);
 
     const _subscribe = async () => {
-        await Pedometer.requestPermissionsAsync();
-        Pedometer.watchStepCount(result => {
-            setCurrentStepCount(result.steps)
-        }),
-            Pedometer.isAvailableAsync().then(
-                result => {
-                    setIsPedometerAvailable(result)
-                },
-                error => {
-                    this.setState({
-                        isPedometerAvailable: 'Could not get isPedometerAvailable: ' + error,
-                    });
-                }
-            )
+        //await Pedometer.requestPermissionsAsync();
+        // Pedometer.watchStepCount(result => {
+        //     setCurrentStepCount(result.steps)
+        // })
+            // Pedometer.isAvailableAsync().then(
+            //     result => {
+            //         setIsPedometerAvailable(result)
+            //     },
+            //     error => {
+            //         this.setState({
+            //             isPedometerAvailable: 'Could not get isPedometerAvailable: ' + error,
+            //         });
+            //     }
+            // );
 
-        if (Platform.OS == "ios") {
-            const end = new Date();
-            const start = new Date();
-            start.setDate(end.getDate() - 1);
-            Pedometer.getStepCountAsync(start, end).then(
-                result => {
-                    setPastStepCount(result.steps)
-                },
-                error => {
-                    this.setState({
-                        pastStepCount: 'Could not get stepCount: ' + error,
-                    });
-                }
-            );
-        }
+        // if (Platform.OS == "ios") {
+        //     const end = new Date();
+        //     const start = new Date();
+        //     start.setDate(end.getDate() - 1);
+        //     Pedometer.getStepCountAsync(start, end).then(
+        //         result => {
+        //             setPastStepCount(result.steps)
+        //         },
+        //         error => {
+        //             this.setState({
+        //                 pastStepCount: 'Could not get stepCount: ' + error,
+        //             });
+        //         }
+        //     );
+        // }
     };
 
     return (
         <View style={styles.container}>
             <View style={styles.PedometerContainer}>
-                <Text style={styles.text}>Pedometer connection: {isPedometerAvailable ? 'Connected' : 'Not connected'}</Text>
-                {Platform.OS == "ios" && <Text style={styles.text}>Steps taken in the last 24 hours: {pastStepCount}</Text>}
-                <Text style={styles.text}>Walk! And watch this go up: {currentStepCount}</Text>
+                {/* <Text style={styles.text}>Pedometer connection: {isPedometerAvailable ? 'Connected' : 'Not connected'}</Text> */}
+                {/* {Platform.OS == "ios" && <Text style={styles.text}>Steps taken in the last 24 hours: {pastStepCount}</Text>} */}
+                {/* <Text style={styles.text}>Walk! And watch this go up: {currentStepCount}</Text> */}
             </View>
         </View>
     );
